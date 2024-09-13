7d12b7d7205f9ea06559b9ff34912d64428e73c2
CED-b2005 <duqingping2k5@gmail.com>
2024-09-13 12:21:47 +0700

    Web Design Week 8.2

--- array.js
+++ array.js
@@ -1,18 +1,14 @@
-const ePN = document.getElementById("enterPName");
-const ePQ = document.getElementById("enterPQuanl");
-const ePP = document.getElementById("enterPPrice");
-const ePD = document.getElementById("enterPDc");
+const pN = document.getElementById("productName")
+const pQ = document.getElementById("productQuanlity")
+const pP = document.getElementById("productPrice")
 
 const noti1 = document.getElementById("noti1")
 const noti2 = document.getElementById("noti2")
 const noti3 = document.getElementById("noti3")
-const noti4 = document.getElementById("noti4")
 
-const sell_but = document.getElementById("sell")
-const cancel_but = document.getElementById("cancel")
+const add_buttom = document.getElementById("add")
 
-const appear = document.getElementById("infoOutput");
-const tab = document.getElementById("b-content");
+const body_tab = document.getElementById("body-table");
 
 let list = [
     [],
@@ -20,10 +16,9 @@ let list = [
     [],
     [],
     [],
-    []
 ];
 
-let id = ["id", "pN", "q", "up", "d", "sT"];
+let className = ["no", "product", "quality", "price", "total"];
 
 function printNoti(object1, object2) {
     if (object1.value == "") {
@@ -56,70 +51,44 @@ function edit(object) {
 }
 
 function makeEmpty() {
-    ePD.value = null;
-    ePN.value = null;
-    ePP.value = null;
-    ePQ.value = null;
+    pQ.value = null;
+    pN.value = null;
+    pP.value = null;
 
     noti1.innerHTML = null;
     noti2.innerHTML = null;
     noti3.innerHTML = null;
-    noti4.innerHTML = null;
 }
 
-function sell() {
-    if ((ePN.value != "" && ePN.value != null) && (ePD.value >= 0 && ePD.value != "") && (ePP.value >= 0 && ePP.value != "") && (ePQ.value >= 0 && ePQ.value != "")) {
-        list[0].push(list[0].length + 1)
-        list[1].push(ePN.value);
-        list[2].push(ePQ.value);
-        list[3].push(ePP.value);
-        list[4].push(ePD.value);
-        list[5].push(ePQ.value * ePP.value * (100 - ePD.value) / 100)
-        makeEmpty();
-        appear.style.display = "block"
-        printTable();
-    } else {
-        console.log("Have empty")
-    }
-}
-
-function cancel() {
-    cancel_but.addEventListener("click", function() {
-        makeEmpty();
-    })
-}
-
-function pDiscountError() {
-    ePD.addEventListener("input", function() {
-        if (ePP.value != "" && ePP.value != null) {
-            if (ePD.value < 0) {
-                ePD.value = 0;
-            } else if (ePD.value > 100) {
-                ePD.value = 100;
-            }
-        } else {
-            ePD.value = null
-        }
-    })
-}
-
-noti(ePN, noti1)
-noti(ePQ, noti2)
-edit(ePQ)
-noti(ePP, noti3)
-edit(ePP)
-noti(ePD, noti4)
-edit(ePD)
-pDiscountError()
 
 function printTable() {
     var createTr = document.createElement("tr")
-    tab.appendChild(createTr)
+    body_tab.appendChild(createTr)
     var i = list[0].length - 1;
-    for (j = 0; j < id.length; j++) {
+    for (j = 0; j < className.length; j++) {
         var createTd = document.createElement('td');
         createTr.appendChild(createTd);
-        createTd.className = id[j];
+        createTd.className = className[j];
         createTd.textContent = list[j][i]
+    }
+}
+
+noti(pN, noti1)
+noti(pQ, noti2)
+edit(pQ)
+noti(pP, noti3)
+edit(pP)
+
+function add() {
+    if ((pN.value != "" && pN.value != null) && (pQ.value >= 0 && pQ.value != "") && (pP.value >= 0 && pP.value != "")) {
+        list[0].push(list[0].length + 1)
+        list[1].push(pN.value);
+        list[2].push(pQ.value);
+        list[3].push(pP.value);
+        list[4].push(pQ.value * pP.value);
+        makeEmpty();
+        printTable();
+    } else {
+        console.log("Have empty");
     }
 }
